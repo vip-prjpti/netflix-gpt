7f922fc7184e22e7434180ac4dda67729787cc1f
vipul prajapati <prajapativipul0408@gmail.com>
2025-10-23 21:33:25 -0400

    banner  added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-       <img src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-751d-8fbc-b84325ea3035/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="" />
+    <div className='absolute'>
+
+       <img className="w-36" src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-751d-8fbc-b84325ea3035/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+    <Header />
+    <img src="https://assets.nflxext.com/ffe/siteui/vlv3/024bfb98-9e9d-441d-9c5f-d2c798d8e42f/web/CA-en-20251013-TRIFECTA-perspective_b1fb583c-c2b0-43ec-a0b6-c990417da87c_large.jpg" alt="" />
+    </div>
   )
 }
 
